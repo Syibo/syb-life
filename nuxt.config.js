ecb8fac49853050e565cb3b9cd3be91d6583fad9
shenyibo <1335638583@qq.com>
2020-05-13 14:33:01 +0800

    第一次提交

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
+  ],
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    proxy: true, // 表示开启代理
+    prefix: '/api', // 表示给请求url加个前缀 /api
+    credentials: true // 表示跨域请求时是否需要使用凭证
+  },
+  proxy: {
+    '/api': {
+      target: 'http://47.107.48.44:7001', // 目标接口域名
+      changeOrigin: true, // 表示是否跨域
+      pathRewrite: {
+        '^/api': '', // 把 /api 替换成‘’
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^element-ui/],
+    vendor: ['axios'],
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container">
+    <div>
+      <!-- <logo /> -->
+<!--       <h1 class="title">
+        nuxt-sbb
+      </h1> -->
+      <h2 class="subtitle">
+        My wicked Nuxt.js project
+      </h2>
+      <div v-for="(item, index) in list">
+        {{ item.link }}
+      </div>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  async asyncData ({ params }) {
+    const li = [
+      {
+        "link": 3333
+      }
+    ]
+    const { data } = await axios({
+      method: 'post',
+      url: 'http://47.107.48.44:7001/admin/web/article/testPost',
+      data: {
+        name: '5',
+      }
+    })
+    return { list: data.data }
+  },
+  async mounted() {
+    const { data } = await axios({
+      method: 'post',
+      url: '/api/admin/web/article/testPost',
+      data: {
+        name: '5',
+      }
+    })
+    console.log(data)
+    // this.list = data.data;
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
