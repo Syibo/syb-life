c06c75ffba787c4466dfad2ea1fa9b0f23634640
syb <>
2020-05-17 14:58:27 +0800

    tupian

---
+++ components/dialog.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="dialog">
+    hh
+</div>
+</template>
+
+<script>
+export default {
+  name: "Dialog",
+  data() {
+    return {
+		menuGive: false
+    }
+  },
+}
+</script>
+<style scoped>
+.dialog {
+    width: 60%;
+    max-width: 1000px;
+    margin-right: auto;
+    margin-left: auto;
+    outline: 1px solid red;
+}
+</style>
