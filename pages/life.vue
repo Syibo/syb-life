7317ab47add8787c804d7cc0e8a30c769a99037f
shenyibo <1335638583@qq.com>
2020-06-09 10:51:29 +0800

    life type

--- pages/life.vue
+++ pages/life.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="life">
+    <div class="life_type">
+      <div class="life_item" v-for="(item, index) in typeList" :key="index">
+        <el-tooltip class="item" effect="dark" :content="item.content" placement="top">
+          <svg class="icon" aria-hidden="true">
+            <use :xlink:href="item.icon"></use>
+          </svg>
+        </el-tooltip>
+      </div>
+    </div>
     <Article :content="item" v-for="(item, index) in diaryList" :key="index"></Article>
   </div>
 </template>
@@ -14,7 +23,15 @@ export default {
   data() {
     return {
       diaryList: [],
-      total: 0
+      total: 0,
+      typeList: [
+        { name: 'pic', icon: '#icontupian', content: '图片' },
+        { name: 'Css', icon: '#iconmiao', content: '笔记' },
+        { name: 'Javascript', icon: '#iconMusic', content: '好好听歌' },
+        { name: 'Vue', icon: '#iconcat', content: '做我的猫' },
+        { name: 'Element', icon: '#iconlvyou', content: '到处走走' },
+        { name: 'Element', icon: '#iconEggs', content: '认真吃饭' },
+      ]
     }
   },
   async asyncData (context) {
@@ -40,5 +57,22 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.life {
+  .life_type {
+    width: 100%;
+    height: 40px;
+    border-bottom: 1px solid #f9f9f9;
+    display: flex;
+    .life_item {
+      .icon {
+        width: 24px;
+        height: 24px;
+        margin: 0 8px 0 16px;
+      }
+    }
+    .life_item:hover {
+      cursor: pointer;
+    }
+  }
+}
 </style>
